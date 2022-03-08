83976156ff32aa4ffc201c1f118d55e366f14631
unknown <emanuel@keemail.me>
2022-03-08 23:26:07 +0100

    refactoring

--- utils/csv_to_var.py
+++ utils/csv_to_var.py
@@ -1,5 +1,5 @@
-with open('words.csv', 'r') as src:
-  with open('words.js', 'w') as dst:
+with open('./utils/words.csv', 'r') as src:
+  with open('./words.js', 'w') as dst:
     dst.write('exports.csv_str=\'')
     dst.write(src.read()[:-1].replace('\n', '\\r\\n'))
     dst.write('\'')
