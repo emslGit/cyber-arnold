c0c9bf46b8d313c4eafb6f409130cd436f7b3553
unknown <emanuel@keemail.me>
2022-03-08 21:07:03 +0100

    heroku fixes

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 
-const csv_file = require("fs").readFileSync("words.csv", "utf8")
+var sample;
 
 const csvToJson = (csv) => {
   const lines = csv.split('\r\n');
@@ -16,14 +17,25 @@ const csvToJson = (csv) => {
   });
 };
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
-app.get("/api/articles", (req, res) => {
+app.get('/api/sample', (req, res) => {
+  const sample = 'art,de,en\r\ndie,Sonne,sun\r\nder,Junge,boy\r\ndas,Gebäude,building'
+  res.json(csvToJson(sample));
+})
+
+app.get('/api/articles', (req, res) => {
+  try {
+    var csv_file = fs.readFileSync(path.join(__dirname, '/words.csv'), 'utf8');
+  } catch (err) {
+    console.error('ERR', err);
+    return;
+  }
   res.json(csvToJson(csv_file));
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
