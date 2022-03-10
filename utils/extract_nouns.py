e50491cdf154b053202d07914cb6cf7f1f03a8bc
unknown <emanuel@keemail.me>
2022-03-10 10:46:28 +0100

    csv

--- utils/extract_nouns.py
+++ utils/extract_nouns.py
@@ -5,7 +5,7 @@ END=1297
 
 df = pd.read_csv('./utils/german-english-word-list-1498-nouns-27900-word-list.csv', usecols= [0, 1])
 de = df.iloc[START:END, 0].str.split(' ', 2, expand=True)
-en = df.iloc[START:END, 1].str.split(' ', 1, expand=True)
+en = df.iloc[START:END, 1].str.split('[', 1, expand=True)
 nouns = pd.concat([de.iloc[:, :2], en.iloc[:, 0]], axis=1)
 nouns.columns = ['art', 'de', 'en']
 nouns.to_csv('./utils/words.csv', index = False)
