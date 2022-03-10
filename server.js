1911b1949bfd2ba67ba3c617f759ff634c2f7e1d
unknown <emanuel@keemail.me>
2022-03-10 09:48:50 +0100

    postgres

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const csvStr = require('./words.js')['csv_str'];
-const assert = require('assert');
 const bodyParser = require('body-parser');
-// const mongo = require('mongodb');
-// const url = 'mongodb://localhost:27017 '
+const { Pool } = require('pg');
 
 var stats = {};
 
-const sample ='art,de,en\r\ndie,Sonne,sun\r\nder,Junge,boy\r\ndas,Gebäude,building\r\nder,Staat,country\r\ndie,Straße,street';
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+/* POSTGRESQL */
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+/* CSV/JSON PARSING */
+const sample = 'art,de,en\r\ndie,Sonne,sun\r\nder,Junge,boy\r\ndas,Gebäude,building\r\nder,Staat,country\r\ndie,Straße,street';
+const csvStr = require('./words.js')['csv_str'];
 
 const csvToJson = (csv) => {
   const lines = csv.split('\r\n');
@@ -23,14 +32,13 @@ const csvToJson = (csv) => {
   })
 }
 
-app.use(express.static(path.join(__dirname, '/client/build')));
-
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+/* API REQUESTS */
 app.post('/api/stats', (req, res) => {
   stats = req.body;
   res.json(req.body);
@@ -49,14 +57,29 @@ app.get('/api/articles', (req, res) => {
       return !excludeWords.includes(word.de);
     });
   }
-  
+
   res.json(json);
+})
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM nouns');
+    const results = { 'results': (result) ? result.rows : null };
+    res.send(JSON.stringify(results));
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
 })
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
 })
 
+
+/* RUNNING */
 const port = process.env.PORT || 5000;
 app.listen(port);
 
